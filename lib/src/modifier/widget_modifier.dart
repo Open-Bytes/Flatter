54189e49b73d85dbd37b4e9fb40bfc3292f021f6
Shaban <sh3ban.kamel@gmail.com>
2022-08-29 23:51:39 +0200

    Improve WidgetModififer documentation

--- lib/src/modifier/widget_modifier.dart
+++ lib/src/modifier/widget_modifier.dart
@@ -1,8 +1,8 @@
 part of '../../flatter.dart';
 
 
-/// A modifier that you apply to a view or another view modifier, producing a different version of the original value.
-/// Adopt the ViewModifier protocol when you want to create a reusable modifier that you can apply to any view.
+/// A modifier that you apply to a widget or another widget modifier, producing a different version of the original value.
+/// Adopt the WidgetModifier protocol when you want to create a reusable modifier that you can apply to any widget.
 /// The example below combines several modifiers to create a new modifier that you can use to add background:
 ///
 /// class BackgroundModifier implements WidgetModifier<Widget, DecoratedBox> {
@@ -21,13 +21,13 @@ part of '../../flatter.dart';
 ///   }
 /// }
 ///
-/// You can apply modifier(_:) directly to a view, but a more common and
-/// idiomatic approach uses modifier(_:) to define an extension to View
-/// itself that incorporates the view modifier:
+/// You can apply modifier(_:) directly to a widget, but a more common and
+/// idiomatic approach uses modifier(_:) to define an extension to widget
+/// itself that incorporates the widget modifier:
 ///
 /// extension BackgroundExt on Widget {
 ///   DecoratedBox background(Color color) {
-///     return widgetModifier(BackgroundModifier(color));
+///     return modifier(BackgroundModifier(color));
 ///   }
 /// }
 abstract class WidgetModifier<Content extends Widget, Body extends Widget> {
